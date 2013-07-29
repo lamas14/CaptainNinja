2a3ec33df7ff31d1d36acec6d3f2693c5a166bcd
lamas14 <lamas14@mycu.concord.edu>
2013-07-28 20:03:49 -0400

    Created Player, Enemy
    Classes created: Player, enemy, fireball, spikes, Animation

--- src/Entity/MapObject.java
+++ src/Entity/MapObject.java
@@ -1,5 +1,6 @@
 package Entity;
 
+import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
 import Main.GamePanel;
@@ -7,45 +8,45 @@ import TileMap.Tile;
 import TileMap.TileMap;
 
 public abstract class MapObject {
-
+	
 	// tile stuff
 	protected TileMap tileMap;
 	protected int tileSize;
-	protected double xMap;
-	protected double yMap;
-
+	protected double xmap;
+	protected double ymap;
+	
 	// position and vector
 	protected double x;
 	protected double y;
 	protected double dx;
 	protected double dy;
-
+	
 	// dimensions
 	protected int width;
 	protected int height;
-
+	
 	// collision box
-	protected int cWidth;
-	protected int cHeight;
-
+	protected int cwidth;
+	protected int cheight;
+	
 	// collision
-	protected int currentRow;
-	protected int currentCol;
-	protected double xDestination;
-	protected double yDestination;
-	protected double xTemp;
-	protected double yTemp;
+	protected int currRow;
+	protected int currCol;
+	protected double xdest;
+	protected double ydest;
+	protected double xtemp;
+	protected double ytemp;
 	protected boolean topLeft;
 	protected boolean topRight;
 	protected boolean bottomLeft;
 	protected boolean bottomRight;
-
-	// Animation
+	
+	// animation
 	protected Animation animation;
 	protected int currentAction;
 	protected int previousAction;
 	protected boolean facingRight;
-
+	
 	// movement
 	protected boolean left;
 	protected boolean right;
@@ -53,8 +54,8 @@ public abstract class MapObject {
 	protected boolean down;
 	protected boolean jumping;
 	protected boolean falling;
-
-	// movement attributes (physics)
+	
+	// movement attributes
 	protected double moveSpeed;
 	protected double maxSpeed;
 	protected double stopSpeed;
@@ -62,146 +63,178 @@ public abstract class MapObject {
 	protected double maxFallSpeed;
 	protected double jumpStart;
 	protected double stopJumpSpeed;
-
+	
 	// constructor
 	public MapObject(TileMap tm) {
 		tileMap = tm;
-		tileSize = tm.getTileSize();
+		tileSize = tm.getTileSize(); 
 	}
-
+	
 	public boolean intersects(MapObject o) {
 		Rectangle r1 = getRectangle();
 		Rectangle r2 = o.getRectangle();
 		return r1.intersects(r2);
 	}
-
+	
 	public Rectangle getRectangle() {
-		return new Rectangle((int) x - cWidth, (int) y - cHeight, cWidth,
-				cHeight);
+		return new Rectangle(
+				(int)x - cwidth,
+				(int)y - cheight,
+				cwidth,
+				cheight
+		);
 	}
-
+	
 	public void calculateCorners(double x, double y) {
-
-		int leftTile = ((int) x - cWidth / 2) / tileSize;
-		int rightTile = ((int) x + cWidth / 2 - 1) / tileSize;
-		int topTile = ((int) y - cHeight / 2) / tileSize;
-		int bottomTile = ((int) y + cHeight / 2 - 1) / tileSize;
-
+		
+		int leftTile = (int)(x - cwidth / 2) / tileSize;
+		int rightTile = (int)(x + cwidth / 2 - 1) / tileSize;
+		int topTile = (int)(y - cheight / 2) / tileSize;
+		int bottomTile = (int)(y + cheight / 2 - 1) / tileSize;
+		
 		int tl = tileMap.getType(topTile, leftTile);
 		int tr = tileMap.getType(topTile, rightTile);
 		int bl = tileMap.getType(bottomTile, leftTile);
 		int br = tileMap.getType(bottomTile, rightTile);
-
+		
 		topLeft = tl == Tile.BLOCKED;
 		topRight = tr == Tile.BLOCKED;
 		bottomLeft = bl == Tile.BLOCKED;
 		bottomRight = br == Tile.BLOCKED;
-
+		
 	}
-
+	
 	public void checkTileMapCollision() {
-		currentCol = (int) x / tileSize;
-		currentRow = (int) y / tileSize;
-
-		xDestination = x + dx;
-		yDestination = y + dy;
-
-		xTemp = x;
-		yTemp = y;
-
-		calculateCorners(x, yDestination);
-		if (dy < 0) {
-			if (topLeft || topRight) {
+		
+		currCol = (int)x / tileSize;
+		currRow = (int)y / tileSize;
+		
+		xdest = x + dx;
+		ydest = y + dy;
+		
+		xtemp = x;
+		ytemp = y;
+		
+		calculateCorners(x, ydest);
+		if(dy < 0) {
+			if(topLeft || topRight) {
 				dy = 0;
-				yTemp = currentRow * tileSize + cHeight / 2;
-			} else {
-				yTemp += dy;
+				ytemp = currRow * tileSize + cheight / 2;
+			}
+			else {
+				ytemp += dy;
 			}
 		}
-		if (dy > 0) {
-			if (bottomLeft || bottomRight) {
+		if(dy > 0) {
+			if(bottomLeft || bottomRight) {
 				dy = 0;
-				yTemp = (currentRow + 1) * tileSize - cHeight / 2;
-			} else {
-				yTemp += dy;
+				falling = false;
+				ytemp = (currRow + 1) * tileSize - cheight / 2;
+			}
+			else {
+				ytemp += dy;
 			}
 		}
-
-		calculateCorners(xDestination, y);
-		if (dx < 0) {
-			if (topLeft || bottomLeft) {
+		
+		calculateCorners(xdest, y);
+		if(dx < 0) {
+			if(topLeft || bottomLeft) {
 				dx = 0;
-				xTemp = currentCol * tileSize + cWidth / 2;
-			} else {
-				xTemp += dx;
+				xtemp = currCol * tileSize + cwidth / 2;
+			}
+			else {
+				xtemp += dx;
 			}
 		}
-		if (dx > 0) {
-			if (topRight || bottomRight) {
+		if(dx > 0) {
+			if(topRight || bottomRight) {
 				dx = 0;
-				xTemp = (currentCol + 1) * tileSize - cWidth / 2;
-			} else {
-				xTemp += dx;
+				xtemp = (currCol + 1) * tileSize - cwidth / 2;
+			}
+			else {
+				xtemp += dx;
 			}
 		}
-
-		if (!falling) {
-			calculateCorners(x, yDestination + 1);
-			if (!bottomLeft && !bottomRight) {
+		
+		if(!falling) {
+			calculateCorners(x, ydest + 1);
+			if(!bottomLeft && !bottomRight) {
 				falling = true;
 			}
 		}
+		
 	}
-
-	public int getx() {
-		return (int) x;
-	}
-
-	public int gety() {
-		return (int) y;
-	}
-
-	public int getWidth() {
-		return width;
-	}
-
-	public int getHeight() {
-		return height;
-	}
-
-	public int getcWidth() {
-		return cWidth;
-	}
-
-	public int getcHeight() {
-		return cHeight;
-	}
-
-	public void setPosition(double x, double y){
+	
+	public int getx() { return (int)x; }
+	public int gety() { return (int)y; }
+	public int getWidth() { return width; }
+	public int getHeight() { return height; }
+	public int getCWidth() { return cwidth; }
+	public int getCHeight() { return cheight; }
+	
+	public void setPosition(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
-	
-	public void setVector(double dx, double dy){
+	public void setVector(double dx, double dy) {
 		this.dx = dx;
 		this.dy = dy;
 	}
 	
-	public void setMapPosition(){
-		xMap = tileMap.getx();
-		yMap = tileMap.gety();
+	public void setMapPosition() {
+		xmap = tileMap.getx();
+		ymap = tileMap.gety();
 	}
 	
 	public void setLeft(boolean b) { left = b; }
-	public void setRight(boolean b) { right = b;}
-	public void setUp(boolean b) { up = b;}
-	public void setDown(boolean b) { down = b;}
-	public void setJumping(boolean b) { jumping = b;}
+	public void setRight(boolean b) { right = b; }
+	public void setUp(boolean b) { up = b; }
+	public void setDown(boolean b) { down = b; }
+	public void setJumping(boolean b) { jumping = b; }
 	
-	public boolean notOnScreen(){
-		return x + xMap + width < 0 || 
-				x + xMap - width > GamePanel.WIDTH ||
-				y + yMap + height < 0 ||
-				y + yMap - height > GamePanel.HEIGHT;
+	public boolean notOnScreen() {
+		return x + xmap + width < 0 ||
+			x + xmap - width > GamePanel.WIDTH ||
+			y + ymap + height < 0 ||
+			y + ymap - height > GamePanel.HEIGHT;
+	}
+	
+	
+	public void draw(Graphics2D g){
+		if(facingRight) {
+			g.drawImage(
+				animation.getImage(),
+				(int)(x + xmap - width / 2),
+				(int)(y + ymap - height / 2),
+				null
+			);
+		}
+		else {
+			g.drawImage(
+				animation.getImage(),
+				(int)(x + xmap - width / 2 + width),
+				(int)(y + ymap - height / 2),
+				-width,
+				height,
+				null
+			);
+			
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/GameState/Level1State.java
+++ src/GameState/Level1State.java
@@ -1,20 +1,29 @@
 package GameState;
 
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
+import Entity.Enemy;
+import Entity.Player;
+import Entity.Enemies.Spikes;
+import Main.GamePanel;
 import TileMap.Background;
 import TileMap.TileMap;
 
-public class Level1State extends GameState{
+public class Level1State extends GameState {
 	
 	private TileMap tileMap;
 	private Background bg;
 	
-	public Level1State(GameStateManager gsm){
+	private Player player;
+	
+	private ArrayList<Enemy> enemies;
+	
+	public Level1State(GameStateManager gsm) {
 		this.gsm = gsm;
 		init();
 	}
-
 	
 	public void init() {
 		
@@ -22,32 +31,89 @@ public class Level1State extends GameState{
 		tileMap.loadTiles("/Tilesets/ninjatileset.gif");
 		tileMap.loadMap("/Maps/level1-1.map");
 		tileMap.setPosition(0, 0);
+		tileMap.setTween(1);
 		
-		bg = new Background("/Backgrounds/levelbg1.gif", 0.1);
+		bg = new Background("/Backgrounds/levelbg1.gif", 0.05);
+		
+		player = new Player(tileMap);
+		player.setPosition(100, 100);
+		
+		enemies = new ArrayList<Enemy>();
+		Spikes s;
+		s = new Spikes(tileMap);
+		s.setPosition(100,100);
+		enemies.add(s);
 		
 	}
-
+	
+	
 	public void update() {
 		
+		// update player
+		player.update();
+		tileMap.setPosition(
+			GamePanel.WIDTH / 2 - player.getx(),
+			GamePanel.HEIGHT / 2 - player.gety()
+		);
+		
+		// set background
+		bg.setPosition(tileMap.getx(), tileMap.gety());
+		
+		// update all enemies
+		for(int i = 0; i < enemies.size(); i++){
+			enemies.get(i).update();
+		}
 	}
-
+	
 	public void draw(Graphics2D g) {
-		//draw Background
+		
+		// draw bg
 		bg.draw(g);
 		
-		//draw tilemap
+		// draw tilemap
 		tileMap.draw(g);
 		
-	}
-
-	public void keyPressed(int k) {
+		// draw player
+		player.draw(g);
+		
+		// draw enemies
+		for(int i = 0; i < enemies.size(); i++){
+			enemies.get(i).draw(g);
+		}
 		
 	}
-
-	public void keyReleased(int k) {
-		
-	}
-
 	
+	public void keyPressed(int k) {
+		if(k == KeyEvent.VK_LEFT) player.setLeft(true);
+		if(k == KeyEvent.VK_RIGHT) player.setRight(true);
+		if(k == KeyEvent.VK_UP) player.setUp(true);
+		if(k == KeyEvent.VK_DOWN) player.setDown(true);
+		if(k == KeyEvent.VK_W) player.setJumping(true);
+		if(k == KeyEvent.VK_E) player.setGliding(true);
+		if(k == KeyEvent.VK_R) player.setScratching();
+		if(k == KeyEvent.VK_F) player.setFiring();
+	}
+	
+	public void keyReleased(int k) {
+		if(k == KeyEvent.VK_LEFT) player.setLeft(false);
+		if(k == KeyEvent.VK_RIGHT) player.setRight(false);
+		if(k == KeyEvent.VK_UP) player.setUp(false);
+		if(k == KeyEvent.VK_DOWN) player.setDown(false);
+		if(k == KeyEvent.VK_W) player.setJumping(false);
+		if(k == KeyEvent.VK_E) player.setGliding(false);
+		
+	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
